58c0ea80d35a79da935443bcf6dddd5a3088f351
juanadls <eleo.dev09@gmail.com>
2024-11-09 19:01:10 -0300

    style of app added

--- lib/src/screens/home_page.dart
+++ lib/src/screens/home_page.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner_app/src/widgets/custom_navigatorbar.dart';
+import 'package:qr_scanner_app/src/widgets/scan_buttom.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Page"),
-      ),
+    return Scaffold(
+      appBar: AppBar(elevation: 0, title: const Text("Historial"), actions: [
+        IconButton(onPressed: () {}, icon: const Icon(Icons.delete_forever))
+      ]),
+      body: const Center(child: Text("Home Page")),
+      bottomNavigationBar: const CustomNavigationBar(),
+      floatingActionButton: const ScanButtom(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
