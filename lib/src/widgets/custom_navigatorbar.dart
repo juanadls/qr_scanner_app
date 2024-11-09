2ad9ad037a90dc3672c66554e6bcfae484afa6b0
juanadls <eleo.dev09@gmail.com>
2024-11-09 19:39:41 -0300

    selectedMenuOpt added

--- lib/src/widgets/custom_navigatorbar.dart
+++ lib/src/widgets/custom_navigatorbar.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_scanner_app/src/providers/ui_provider.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   const CustomNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const currentIndex = 0;
+    final uiprovider = Provider.of<UiProvider>(context);
+    final currentIndex = uiprovider.selectedMenuOpt;
     return BottomNavigationBar(
+      onTap: (int index) {
+        uiprovider.selectedMenuOpt = index;
+      },
       elevation: 0,
       currentIndex: currentIndex,
       items: const [
